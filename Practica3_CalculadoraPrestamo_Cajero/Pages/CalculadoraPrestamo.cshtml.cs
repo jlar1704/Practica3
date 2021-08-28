0de9202bde3fecb75b75200dbb2632b04f317cf4
jlar17 <juanluisabreu@gmail.com>
2021-08-28 12:58:23 -0400

    first commit

---
+++ Practica3_CalculadoraPrestamo_Cajero/Pages/CalculadoraPrestamo.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practica3_CalculadoraPrestamo_Cajero.Pages
+{
+    public class CalculadoraPrestamoModel : PageModel
+    {
+        public double CuotaMensual { get; set; }
+
+        public void OnGet(double MontoPrestamo, int CantidadCuotas, double PorcentajeInteres)
+        {
+            this.CuotaMensual = MontoPrestamo * (1 + (((CantidadCuotas / 12) * PorcentajeInteres) / 100)) / (CantidadCuotas);
+        }
+    }
+}
